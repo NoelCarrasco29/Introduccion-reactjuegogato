f56fc8a17e07199a3aca1a1fd13cacbec8f14c6b
Noel Carrasco <carrascon3055@gmail.com>
2022-05-12 02:21:58 -0600

    commit para cambiar el texo del status en el render de la clase board

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -37,7 +37,8 @@ function Square(props) {
     }
 
     render() {
-      const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
   
       return (
         <div>
